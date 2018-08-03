afe85d789b4d2f7a7b1376d5204e9305277881ea
vid podpecan <vid.podpecan@ijs.si>
2018-08-03 13:13:46 +0200

    few fixes

--- pages/admin.py
+++ pages/admin.py
@@ -5,7 +5,7 @@ from pages import models
 
 class HTML_page_admin(admin.ModelAdmin):
     fields = ('name', 'content')
-    readonly_fields = ('name',)
+    # readonly_fields = ('name',)
 
 
 admin.site.register(models.HTML_page, HTML_page_admin)
